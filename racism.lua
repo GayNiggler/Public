fc4185d0c541a5bb663a72418f96a4eac85700f4
GayNiggler <129532994+GayNiggler@users.noreply.github.com>
2023-05-01 03:17:13 -0400

    Create racism.lua

---
+++ racism.lua
@@ -0,0 +1,28 @@
+_G.autoTap = true
+while _G.autoTap == true do
+    local yes = nil
+    
+        yes = true
+        while yes and wait() do
+        local mod = game:GetService("Players").LocalPlayer.Backpack.Ak47.ConfigMods.CConfig
+        local ni = require(mod)
+        ni.gunRecoilMin = 0
+	    ni.gunRecoilMax = 0
+	    ni.AimGunRecoilMin = 0 
+	    ni.AimGunRecoilMax = 0 
+	    ni.KickbackMin = 0
+	    ni.KickbackMax = 0
+	    ni.AimKickbackMin = 0 
+	    ni.AimKickbackMax = 0
+     	ni.SideKickMin = 0
+	    ni.SideKickMax = 0
+	    ni.AimSideKickMin = 0
+	    ni.AimSideKickMax = 0
+	    ni.CamShakeMin = 0
+	    ni.CamShakeMax = 0 
+	    ni.AimCanShakeMin = 0
+	    ni.AimCamShakeMax = 0
+        end
+
+	wait()
+end
