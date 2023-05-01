9ac290700ba23fcd74c24f0d191f300e2a3a006f
GayNiggler <129532994+GayNiggler@users.noreply.github.com>
2023-05-01 03:32:15 -0400

    Update racism.lua

--- racism.lua
+++ racism.lua
@@ -22,7 +22,33 @@ while _G.autoTap == true do
 	    ni.CamShakeMax = 0 
 	    ni.AimCanShakeMin = 0
 	    ni.AimCamShakeMax = 0
+   wait ()
+		local yes = nil
+    
+        yes = true
+        while yes and wait() do
+        local mod = game:GetService("Players").LocalPlayer.Backpack.Ak47H.ConfigMods.CConfig
+        local ni = require(mod)
+        ni.gunRecoilMin = 0
+	    ni.gunRecoilMax = 0
+	    ni.AimGunRecoilMin = 0 
+	    ni.AimGunRecoilMax = 0 
+	    ni.KickbackMin = 0
+	    ni.KickbackMax = 0
+	    ni.AimKickbackMin = 0 
+	    ni.AimKickbackMax = 0
+     	ni.SideKickMin = 0
+	    ni.SideKickMax = 0
+	    ni.AimSideKickMin = 0
+	    ni.AimSideKickMax = 0
+	    ni.CamShakeMin = 0
+	    ni.CamShakeMax = 0 
+	    ni.AimCanShakeMin = 0
+	    ni.AimCamShakeMax = 0
         end
+
+	end
+
 
 	wait()
 end
